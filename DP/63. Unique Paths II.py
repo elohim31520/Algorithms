7f99a9962c7ca6c39a8ab3a291cc89a7a8884854
elohim31520 <versus7063@gmail.com>
2023-04-06 15:33:50 +0800

    add leetcode 63

---
+++ DP/63. Unique Paths II.py
@@ -0,0 +1,51 @@
+# 63. Unique Paths II
+# Medium
+# 6.8K
+# 433
+# Companies
+# You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+# An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+
+# Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+# The testcases are generated so that the answer will be less than or equal to 2 * 109.
+
+# Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+# Output: 2
+# Explanation: There is one obstacle in the middle of the 3x3 grid above.
+# There are two ways to reach the bottom-right corner:
+# 1. Right -> Right -> Down -> Down
+# 2. Down -> Down -> Right -> Right
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: list[list[int]]) -> int:
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        dp = [[0 for _ in range(m)] for _ in range(n)]
+        dp[0][0] == 0 if obstacleGrid[0][0] == 1 else 0
+        for i in range(m):
+            if obstacleGrid[i][0] == 0:
+                dp[i][0] = 1
+            else:
+                break
+        for j in range(n):
+            if obstacleGrid[0][j] == 0:
+                dp[0][j] = 1
+            else: 
+                break
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] == 0:
+                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+                else:
+                    dp[i][j] = 0
+        print(dp)
+        return dp[i][j]
+    
+
+solution = Solution()
+# 调用函数
+count = solution.uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]])
+
+print(count)
